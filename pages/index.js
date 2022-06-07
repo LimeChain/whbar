e74d26f447518ab42e2526b9c5f0cd23e625a083
Martin Dobrev <martindobrev87@gmail.com>
2022-06-07 09:51:04 +0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="container py-5 py-lg-7">
+      <h1>WHBAR Landing page</h1>
+    </div>
+  );
+}
