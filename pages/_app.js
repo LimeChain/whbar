bf249ab884e83d4b180f253599298dd7e2eba951
Martin Dobrev <martindobrev87@gmail.com>
2022-06-23 16:57:55 +0300

    GA added
    Signed-off-by: Martin Dobrev <martindobrev87@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,31 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../styles/styles.scss';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-TZTBG5ZWW3`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TZTBG5ZWW3', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
